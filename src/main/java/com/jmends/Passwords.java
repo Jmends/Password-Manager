28e501ebaa84d5e9a386121af62d320704294bde
Jmends <jonathanamends7@gmail.com>
2024-09-13 22:40:45 -0500

    created password generator method

--- src/main/java/com/jmends/Passwords.java
+++ src/main/java/com/jmends/Passwords.java
@@ -1,5 +1,45 @@
 package com.jmends;
 
+import java.util.Random;
+
 public class Passwords {
-    
+    public String generatePassword() {
+        Random random = new Random();
+        String[] letters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+                "U", "V", "W", "X", "Y", "Z",
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+                "u", "v", "w", "x", "y", "z"};
+
+        String[] numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String[] symbols = {"!", "@", "#", "$", "%", "^", "&", "*", "(", ")",
+                "-", "_", "=", "+", "[", "]", "{", "}", ";", ":",
+                "'", "\"", "<", ">", ",", ".", "/", "?"};
+
+        String[] allCharacters = new String[letters.length + numbers.length + symbols.length];
+        int index = 0;
+
+
+        for (String letter : letters) {
+            allCharacters[index++] = letter;
+        }
+        for (String number : numbers) {
+            allCharacters[index++] = number;
+        }
+        for (String symbol : symbols) {
+            allCharacters[index++] = symbol;
+        }
+
+        int passwordLength = 6 + random.nextInt(7);
+
+        StringBuilder password = new StringBuilder();
+
+        for(int i = 0; i < passwordLength; i++){
+            password.append(allCharacters[random.nextInt(allCharacters.length)]);
+        }
+
+        return password.toString();
+
+    }
 }
