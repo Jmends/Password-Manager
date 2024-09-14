0fa125e9d651e14102066537249cae5ee5e047b6
Jmends <jonathanamends7@gmail.com>
2024-09-14 00:02:08 -0500

    test method from Passwords class

--- src/test/java/com/jmends/PasswordsTest.java
+++ src/test/java/com/jmends/PasswordsTest.java
@@ -1,4 +1,17 @@
+package com.jmends;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PasswordsTest {
-  
+
+    @Test
+    void shouldGeneratePassword() {
+        Passwords password = new Passwords();
+
+        String generatedPassword = password.generatePassword();
+
+        assertFalse(generatedPassword.isEmpty(), "Password should not be empty");
+    }
 }
