58f7eec75494d1bd80892ac45399c22afc6d2c32
Jmends <jonathanamends7@gmail.com>
2024-09-20 23:26:50 -0500

    Added login and account creation methods

--- src/main/java/com/jmends/Users.java
+++ src/main/java/com/jmends/Users.java
@@ -3,8 +3,7 @@ package com.jmends;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
 
@@ -26,7 +25,55 @@ public class Users {
         return id;
     }
 
-    public static String hashPassword(String password) throws NoSuchAlgorithmException {
+    public String getUsername() {
+        return username;
+    }
+
+    public static Users login(String username,String password) throws SQLException {
+        try(Connection conn = DatabaseHandler.getConnection()){
+            String hashedPassword = hashPassword(password);
+            String sql = "SELECT id, username FROM users WHERE username = ? AND password_hash = ?";
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1,username);
+            pstmt.setString(2,hashedPassword);
+            ResultSet rs = pstmt.executeQuery();
+
+            if(rs.next()){
+                return new Users(rs.getInt("id"),rs.getString("username"));
+            }
+        } catch (SQLException |NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+
+    }
+
+
+    public static Boolean createAccount(String username, String password)  {
+        String hashedPassword;
+        try {
+            hashedPassword = hashPassword(password);
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("ERROR HASHING PASSWORD: " + e.getMessage());
+            return false;
+        }
+        String sql = "INSERT INTO users(username,password_hash) VALUES (?,?)";
+
+
+        try(Connection conn = DatabaseHandler.getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1,username);
+            pstmt.setString(2,hashedPassword);
+            pstmt.executeUpdate();
+            return true;
+        } catch (SQLException e){
+            System.out.println("ERROR REGISTERING USER: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public static String hashPassword(String password) throws NoSuchAlgorithmException{
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         byte[] hash = md.digest(password.getBytes());
         StringBuilder hexString = new StringBuilder();
@@ -34,29 +81,6 @@ public class Users {
             hexString.append(String.format("%02x", b));
         }
         return hexString.toString();
-    }
-
-    public void createAccount(String username, String password) throws NoSuchAlgorithmException {
-        String hashedPassword = hashPassword(password);
-
-        String sql = "INSERT INTO users (username,password) VALUES (?,?)";
-        try (Connection conn = DatabaseHandler.getConnection();
-             PreparedStatement psmt = conn.prepareStatement(sql)) {
-            psmt.setString(1, username);
-            psmt.setString(2, hashedPassword);
-
-            int rowsAffected = psmt.executeUpdate();
-
-            if (rowsAffected > 0) {
-                System.out.println("Account creation successful!");
-            } else {
-                System.out.println("Account creation failed");
-            }
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
     }
 
 }
