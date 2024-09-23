9a0e3cd8a626880867b80584b3db7ef7f3d7516f
JMends <Jonathanamends7@gmail.com>
2024-09-22 21:30:14 -0500

    Started main section of the program

--- src/main/java/com/jmends/Main.java
+++ src/main/java/com/jmends/Main.java
@@ -1,10 +1,24 @@
 package com.jmends;
+import java.util.Scanner;
 
 public class Main {
+
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
     DatabaseHandler.initializeDatabase();
 
+    while(true){
+        System.out.println("1. Register User\n2. Login\n3. Exit");
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // handles next line trap
+
+        if (choice == 1){
+            System.out.println("hi");
+            break;
+            // register user
+        }
+    }
 
     }
 }
